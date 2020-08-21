7ad9179ed1fab483986cdb1e0e7f5b26f00e2fd9
Juan Palta <juanpalta24@hotmail.com>
2020-08-21 18:33:29 -0500

    add default serial version id

--- src/exceptions/NoDayException.java
+++ src/exceptions/NoDayException.java
@@ -2,6 +2,11 @@ package exceptions;
 
 public class NoDayException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public NoDayException() {
 		
 		super("TODAY IS NOT YOUR DEPARTURE DAY ");
--- src/exceptions/NoValidOptionException.java
+++ src/exceptions/NoValidOptionException.java
@@ -2,6 +2,11 @@ package exceptions;
 
 public class NoValidOptionException extends Exception{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public NoValidOptionException() {
 		
 		super("YOU DONT INSERT A VALID OPTION ");
